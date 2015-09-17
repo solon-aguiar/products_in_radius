e43697944db62b014ea0d30f1f2992f4fbbdfa60
Solon Aguiar <solon.liam.aguiar@gmail.com>
2015-09-16 20:40:51 -0700

    Tests for the search with tags

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -74,3 +74,29 @@ class TestPopularProductsFinder:
         assert len(sport_products) == 1
         assert sport_products[0].title == "nike tiempo"
 
+        all_products = finder.most_popular_products(self.search_coordinates,self.search_radius,["sports","books"],1000)
+        assert len(all_products) == 5
+
+    def test_does_not_return_twice_the_product_from_a_store_if_it_matches_both_tags(self):
+        shop = Shop("close shop",59.33265,18.06061)
+        shop.add_product(Product("harry potter",0.9))
+        shop.add_product(Product("harry potter II",0.8))
+        shop.add_product(Product("Ibrahimovic jersey",0.9))
+
+        closer_shop = Shop("shop nearby",59.33265,18.06364)
+        closer_shop.add_product(Product("nike tiempo",0.9))
+
+        boundary_shop = Shop("shop in the boundary",59.33717,18.06637)
+        boundary_shop.add_product(Product("Game of Thrones",0.1))
+        boundary_shop.add_product(Product("Hunger Games",0.3))
+
+        finder = PopularProductsFinder({"books":[shop,boundary_shop],"sports":[closer_shop,shop]},[shop,closer_shop,boundary_shop])
+
+        products = finder.most_popular_products(self.search_coordinates,self.search_radius,[],1000)
+        assert len(products) == 6
+        assert products[0].title == "nike tiempo"
+        assert products[1].title == "Ibrahimovic jersey"
+        assert products[2].title == "harry potter"
+        assert products[3].title == "harry potter II"
+        assert products[4].title == "Hunger Games"
+        assert products[5].title == "Game of Thrones"
